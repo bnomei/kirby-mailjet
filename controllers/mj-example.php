dcdbb7c61ced4dccec80505593f215699051bdbd
Bruno Meilick <b@bnomei.com>
2017-05-22 18:21:49 +0200

    fixed indention
    Signed-off-by: Bruno Meilick <b@bnomei.com>

--- controllers/mj-example.php
+++ controllers/mj-example.php
@@ -4,9 +4,9 @@ use Uniform\Form;
 
 return function($site, $pages, $page) {
 
-	/////////////////////////////////////
-	// Opt-In Uniform
-	$form = new Form([
+    /////////////////////////////////////
+    // Opt-In Uniform
+    $form = new Form([
         'firstname' => [
             'rules'   => ['required'],
             'message' => 'Please enter your firstname',
@@ -22,54 +22,54 @@ return function($site, $pages, $page) {
     ]);
 
     if (r::is('POST') && 
-    	$form->honeypotGuard() && // perform at least one action/check...
-    	$form->success() // ... now success can be determined
+        $form->honeypotGuard() && // perform at least one action/check...
+        $form->success() // ... now success can be determined
     ) {
 
-    	$formFirstname = strip_tags( $form->data('firstname') );
-    	$formLastname  = strip_tags( $form->data('lastname') );
-    	$formEmail     = strip_tags( $form->data('email') );
+        $formFirstname = strip_tags( $form->data('firstname') );
+        $formLastname  = strip_tags( $form->data('lastname') );
+        $formEmail     = strip_tags( $form->data('email') );
 
-    	// Firstname Lastname <some@ema.il>
-    	$emailTo = implode(' ', 
-    		[
-	    		$formFirstname,
-	    		$formLastname,
-	    		'<'.$formEmail.'>'
-    		]
-    	);
+        // Firstname Lastname <some@ema.il>
+        $emailTo = implode(' ', 
+            [
+                $formFirstname,
+                $formLastname,
+                '<'.$formEmail.'>'
+            ]
+        );
 
-    	$emailSubject = 'Newsletter Opt-In Link';
+        $emailSubject = 'Newsletter Opt-In Link';
 
-    	$optInLink = implode('',
-    		[
-    			$page->url(),
-    			'?newsletter=optin', // see #OPTIN
-    			'&firstname='.urlencode($formFirstname),
-    			'&lastname='. urlencode($formLastname),
-    			'&email='.    urlencode($formEmail),
-    			// add a token to check data. order is important. see #CHECK
-    			'&token='.sha1(
-    				KirbyMailjet::hash(). // this is unique for your server, never use it alone
-    				date('Ym'). // minimalistic timeout: current month
-    				$formFirstname.
-    				$formLastname.
-    				$formEmail
-    				),
-    		]
-    	);
-    	$data = [
-    		// mj-example-block-headline
-    		'headline' => $emailSubject,
+        $optInLink = implode('',
+            [
+                $page->url(),
+                '?newsletter=optin', // see #OPTIN
+                '&firstname='.urlencode($formFirstname),
+                '&lastname='. urlencode($formLastname),
+                '&email='.    urlencode($formEmail),
+                // add a token to check data. order is important. see #CHECK
+                '&token='.sha1(
+                    KirbyMailjet::hash(). // this is unique for your server, never use it alone
+                    date('Ym'). // minimalistic timeout: current month
+                    $formFirstname.
+                    $formLastname.
+                    $formEmail
+                    ),
+            ]
+        );
+        $data = [
+            // mj-example-block-headline
+            'headline' => $emailSubject,
 
-    		// mj-example-block-text
-    		'text' => '<p><a href="'.$optInLink.'">please click here</a></p>',
-    		'footer' => '',
-    	];
-    	if($htmlfile = KirbyMailjet::buildMJML('mj-example-email')) {
-    		KirbyMailjet::execMJML($htmlfile);
-    	}
-    	$emailBody = KirbyMailjet::renderMustache('mj-example-email', $data);
+            // mj-example-block-text
+            'text' => '<p><a href="'.$optInLink.'">please click here</a></p>',
+            'footer' => '',
+        ];
+        if($htmlfile = KirbyMailjet::buildMJML('mj-example-email')) {
+            KirbyMailjet::execMJML($htmlfile);
+        }
+        $emailBody = KirbyMailjet::renderMustache('mj-example-email', $data);
 
         KirbyMailjet::sendMail([
             'to' => $emailTo,
@@ -84,38 +84,38 @@ return function($site, $pages, $page) {
         ]);
 
         go($page->url().'?newsletter=verify');
-		// or of you have a dedicated page...
-		// go(page('newsletter/verify')->url());
+        // or of you have a dedicated page...
+        // go(page('newsletter/verify')->url());
         
     }
 
     /////////////////////////////////////
-	// Opt-In Link Handler
-	$optinStatus = null;
-	if(get('newsletter') == 'optin') { // #OPTIN
-		$linkFirstname = urldecode(get('firstname'));
-		$linkLastname = urldecode(get('lastname'));
-		$linkEmail = urldecode(get('email'));
-		$token = get('token');
+    // Opt-In Link Handler
+    $optinStatus = null;
+    if(get('newsletter') == 'optin') { // #OPTIN
+        $linkFirstname = urldecode(get('firstname'));
+        $linkLastname = urldecode(get('lastname'));
+        $linkEmail = urldecode(get('email'));
+        $token = get('token');
 
-		$check = sha1( // #CHECK
-			KirbyMailjet::hash(). // this is unique for your server, never use it alone
-			date('Ym').
-			$linkFirstname.
-			$linkLastname.
-			$linkEmail
-		);
+        $check = sha1( // #CHECK
+            KirbyMailjet::hash(). // this is unique for your server, never use it alone
+            date('Ym').
+            $linkFirstname.
+            $linkLastname.
+            $linkEmail
+        );
 
-		if($token == $check) { // success
+        if($token == $check) { // success
 
-			$updatedContactslist = '';
-			if(KirbyMailjet::updateContactslist(
+            $updatedContactslist = '';
+            if(KirbyMailjet::updateContactslist(
                     'Newsletter Test', 
                     KirbyMailjet::LIST_ADDFORCE,
                     [
-                    	'email' => $linkEmail,
-                    	'firstname' => $linkFirstname,
-                    	'lastname' => $linkLastname,
+                        'email' => $linkEmail,
+                        'firstname' => $linkFirstname,
+                        'lastname' => $linkLastname,
                     ]
                 )) {
                 $optinStatus = 'success';
@@ -123,195 +123,195 @@ return function($site, $pages, $page) {
                 $optinStatus = 'failed';
             }
 
-			$emailSubject = 'Newsletter Opt-In '.$updatedContactslist;
+            $emailSubject = 'Newsletter Opt-In '.$updatedContactslist;
 
-	    	$data = [
-	    		// mj-example-block-headline
-	    		'headline' => $emailSubject,
+            $data = [
+                // mj-example-block-headline
+                'headline' => $emailSubject,
 
-	    		// mj-example-block-text
-	    		'text' => '<p>'.$optinStatus.'</p>',
-	    		'footer' => '',
-	    	];
-	    	if($htmlfile = KirbyMailjet::buildMJML('mj-example-email')) {
-	    		KirbyMailjet::execMJML($htmlfile);
-	    	}
-	    	$emailBody = KirbyMailjet::renderMustache('mj-example-email', $data);
+                // mj-example-block-text
+                'text' => '<p>'.$optinStatus.'</p>',
+                'footer' => '',
+            ];
+            if($htmlfile = KirbyMailjet::buildMJML('mj-example-email')) {
+                KirbyMailjet::execMJML($htmlfile);
+            }
+            $emailBody = KirbyMailjet::renderMustache('mj-example-email', $data);
 
-	        KirbyMailjet::sendMail([
-	            'to' => $linkEmail,
-	            'from' => KirbyMailjet::senderAdress(),
-	            'subject' => $emailSubject,
-	            'body' => $emailBody,
-	            'service' => KirbyMailjet::EMAIL_SERVICE, // send with mailjet
-	            'options' => [
-	                // 'Text-part' =>
-	                'Mj-campaign' => $emailSubject
-	            ]
-	        ]);
+            KirbyMailjet::sendMail([
+                'to' => $linkEmail,
+                'from' => KirbyMailjet::senderAdress(),
+                'subject' => $emailSubject,
+                'body' => $emailBody,
+                'service' => KirbyMailjet::EMAIL_SERVICE, // send with mailjet
+                'options' => [
+                    // 'Text-part' =>
+                    'Mj-campaign' => $emailSubject
+                ]
+            ]);
 
-		} else {
-			// data changed or timeout
-			$optinStatus = 'timeout';
-		}
-	}
+        } else {
+            // data changed or timeout
+            $optinStatus = 'timeout';
+        }
+    }
 
-	/////////////////////////////////////
-	// Optin Link Handler Exit
-	if($optinStatus) {
-		// avoid dublicate optin submission using PRG-Pattern: https://wiki2.org/en/Post/Redirect/Get
-		// redirect away from optin link of email
-		go($page->url().'?newsletter='.$optinStatus);
-		// or of you have a dedicated page...
-		// go(page('newsletter/'.$optinStatus)->url());
-	}
+    /////////////////////////////////////
+    // Optin Link Handler Exit
+    if($optinStatus) {
+        // avoid dublicate optin submission using PRG-Pattern: https://wiki2.org/en/Post/Redirect/Get
+        // redirect away from optin link of email
+        go($page->url().'?newsletter='.$optinStatus);
+        // or of you have a dedicated page...
+        // go(page('newsletter/'.$optinStatus)->url());
+    }
 
 
     /////////////////////////////////////
-	// prepare json response
-	$mailjetJSON = ['code' => 400];
+    // prepare json response
+    $mailjetJSON = ['code' => 400];
 
-	$mustache = array();
-	foreach($page->builder()->toStructure() as $data) {
-		$fieldset = $data->_fieldset();
-		$snipjson = json_decode(snippet('mj-example-block-'.$fieldset, [
-				'page' => $page,
-				'data' => $data,
-				'json' => true, // request json from snippet
-			], true), true);
-		if($snipjson) {
-			$mustache = array_merge($mustache, $snipjson);
-		}
-	}
+    $mustache = array();
+    foreach($page->builder()->toStructure() as $data) {
+        $fieldset = $data->_fieldset();
+        $snipjson = json_decode(snippet('mj-example-block-'.$fieldset, [
+                'page' => $page,
+                'data' => $data,
+                'json' => true, // request json from snippet
+            ], true), true);
+        if($snipjson) {
+            $mustache = array_merge($mustache, $snipjson);
+        }
+    }
 
-	$mjmlCode = '';
-	$mjmlFile = kirby()->roots()->site() . DS . 'plugins' . DS . 'kirby-mailjet' . DS . 'snippets' . DS . 'mj-example-newsletter.mjml';
-	if(!f::exists($mjmlFile)) {
-		$mjmlFile = KirbyMailjet::buildMJML('mj-example-newsletter', true); // snippet in plugin folder
-		if($mjmlFile) {
-			KirbyMailjet::execMJML($mjmlFile);
-		}
-		$mjmlCode = f::read($mjmlFile);
-	} else {
-		$mjmlCode = f::read($mjmlFile);
-	}
+    $mjmlCode = '';
+    $mjmlFile = kirby()->roots()->site() . DS . 'plugins' . DS . 'kirby-mailjet' . DS . 'snippets' . DS . 'mj-example-newsletter.mjml';
+    if(!f::exists($mjmlFile)) {
+        $mjmlFile = KirbyMailjet::buildMJML('mj-example-newsletter', true); // snippet in plugin folder
+        if($mjmlFile) {
+            KirbyMailjet::execMJML($mjmlFile);
+        }
+        $mjmlCode = f::read($mjmlFile);
+    } else {
+        $mjmlCode = f::read($mjmlFile);
+    }
 
-	// call from panel kirby-opener button?
-	if((r::ajax() || param('mode') == 'preview') && $site->user()) {
+    // call from panel kirby-opener button?
+    if((r::ajax() || param('mode') == 'preview') && $site->user()) {
 
-		// if mailjet available and kirby-opener secret valid
-		if(KirbyMailjet::client() != null && $page->openerSecret() == param('secret')) {
+        // if mailjet available and kirby-opener secret valid
+        if(KirbyMailjet::client() != null && $page->openerSecret() == param('secret')) {
 
-			$contactslist = null;
-			if($page->mjcontactslist()->isNotEmpty()) {
-				$contactslist = $page->mjcontactslist()->value();
-			} else {
-				$mailjetJSON['message'] = 'Choose Contactslist';
-				
-				die(response::json($mailjetJSON, 200));
-			}
+            $contactslist = null;
+            if($page->mjcontactslist()->isNotEmpty()) {
+                $contactslist = $page->mjcontactslist()->value();
+            } else {
+                $mailjetJSON['message'] = 'Choose Contactslist';
+                
+                die(response::json($mailjetJSON, 200));
+            }
 
-			// determin send mode: test or publish
-			$testEmail = null;
-			if(param('mailjet') == 'send') {
-				// maybe to a role permission check? up to you.
-				$testEmail = 'Publish';	
+            // determin send mode: test or publish
+            $testEmail = null;
+            if(param('mailjet') == 'send') {
+                // maybe to a role permission check? up to you.
+                $testEmail = 'Publish'; 
 
-			} else if(param('mailjet') == 'test') {
-				$mjemail = $page->mjemail();
-				if($mjemail->isNotEmpty()) {
-					$testEmail = $mjemail->value();
-				} else {
-					$testEmail = $site->user()->email();
-				}
-			}
+            } else if(param('mailjet') == 'test') {
+                $mjemail = $page->mjemail();
+                if($mjemail->isNotEmpty()) {
+                    $testEmail = $mjemail->value();
+                } else {
+                    $testEmail = $site->user()->email();
+                }
+            }
 
-			if(param('mailjet') == 'send' || param('mailjet') == 'test') {
+            if(param('mailjet') == 'send' || param('mailjet') == 'test') {
 
-				$title = date('Y-m-d H:i:s').' ['.$site->user()->username().']';
-				$subject = trim($page->title());
-				$pageurl = $page->url();
-				if(strlen($pageurl) > 100) { // mailjet will not accept that for its dashboard
-					$pageurl = $page->tinyurl();
-				}
+                $title = date('Y-m-d H:i:s').' ['.$site->user()->username().']';
+                $subject = trim($page->title());
+                $pageurl = $page->url();
+                if(strlen($pageurl) > 100) { // mailjet will not accept that for its dashboard
+                    $pageurl = $page->tinyurl();
+                }
 
-				$snippet = 'mj-example-newsletter';
-				$html = null;
-				if($htmlfile = KirbyMailjet::buildMJML($snippet)) {
-					KirbyMailjet::execMJML($htmlfile);
-				}
-				$html = KirbyMailjet::renderMustache(
-					$snippet, 
-					$mustache
-				);
+                $snippet = 'mj-example-newsletter';
+                $html = null;
+                if($htmlfile = KirbyMailjet::buildMJML($snippet)) {
+                    KirbyMailjet::execMJML($htmlfile);
+                }
+                $html = KirbyMailjet::renderMustache(
+                    $snippet, 
+                    $mustache
+                );
 
-				$campaign_body = [
-					    'Locale' => "en", // de_DE
-					    'SenderEmail' =>KirbyMailjet::senderAdress(),
-					    'Sender' => KirbyMailjet::senderName(),
-					    'Subject' => $subject, // filter UNIQUE
-					    'Title' => $title,
-					    'Url' => trim($pageurl),
-					];
+                $campaign_body = [
+                        'Locale' => "en", // de_DE
+                        'SenderEmail' =>KirbyMailjet::senderAdress(),
+                        'Sender' => KirbyMailjet::senderName(),
+                        'Subject' => $subject, // filter UNIQUE
+                        'Title' => $title,
+                        'Url' => trim($pageurl),
+                    ];
 
-				if($page->mjsegment()->isNotEmpty()) {
-					$segmentation = $page->mjsegment()->value();
-					$segid = KirbyMailjet::getSegment($segmentation);
-					if($segid) {
-						$campaign_body['SegmentationID'] = $segid;
-					}
-				}
+                if($page->mjsegment()->isNotEmpty()) {
+                    $segmentation = $page->mjsegment()->value();
+                    $segid = KirbyMailjet::getSegment($segmentation);
+                    if($segid) {
+                        $campaign_body['SegmentationID'] = $segid;
+                    }
+                }
 
-				$txt = "If this message is not displayed properly, please visit this website: ".trim($page->url());
+                $txt = "If this message is not displayed properly, please visit this website: ".trim($page->url());
 
-				$campaign_content = [
-					    'Html-part' => $html,
-					    'Text-part' => $txt,
-					];
+                $campaign_content = [
+                        'Html-part' => $html,
+                        'Text-part' => $txt,
+                    ];
 
-				// MODE: send transactional email
-				if(param('mode') == 'transactional') {
-					$emailparams = [
-						'from' => KirbyMailjet::senderAdress(),
-						'to' => $testEmail,
-						'subject' => $subject,
-						'body' => $html,
-						'options' => [
-							'Text-part' => $txt,
-							'Mj-campaign' => trim($page->slug()),
-						]
-					];
-					if(KirbyMailjet::sendMail($emailparams)) {
-						$mailjetJSON['code'] = 200;
-					}
-				}
+                // MODE: send transactional email
+                if(param('mode') == 'transactional') {
+                    $emailparams = [
+                        'from' => KirbyMailjet::senderAdress(),
+                        'to' => $testEmail,
+                        'subject' => $subject,
+                        'body' => $html,
+                        'options' => [
+                            'Text-part' => $txt,
+                            'Mj-campaign' => trim($page->slug()),
+                        ]
+                    ];
+                    if(KirbyMailjet::sendMail($emailparams)) {
+                        $mailjetJSON['code'] = 200;
+                    }
+                }
 
-				// MODE: dump html
-				else if(param('mode') == 'preview') {
-					echo $html; die();
-				}
+                // MODE: dump html
+                else if(param('mode') == 'preview') {
+                    echo $html; die();
+                }
 
-				// MODE: create and send newsletter (test or publish)
-				else {
-					$mailjetJSON = KirbyMailjet::sendNewsletter(
-						$contactslist,
-						$campaign_body,
-						$campaign_content,
-						$testEmail
-					);
-				}
-				
-			} // send or test
+                // MODE: create and send newsletter (test or publish)
+                else {
+                    $mailjetJSON = KirbyMailjet::sendNewsletter(
+                        $contactslist,
+                        $campaign_body,
+                        $campaign_content,
+                        $testEmail
+                    );
+                }
+                
+            } // send or test
 
 
-		} else {
-			$mailjetJSON['message'] = 'Authentication failed.';
-		}
+        } else {
+            $mailjetJSON['message'] = 'Authentication failed.';
+        }
 
-		die(response::json($mailjetJSON, 200)); // reached api
+        die(response::json($mailjetJSON, 200)); // reached api
 
-	// or frontend page view
-	} else {
-		return compact('mustache', 'mjmlCode', 'form');
-	}
+    // or frontend page view
+    } else {
+        return compact('mustache', 'mjmlCode', 'form');
+    }
 };
--- templates/mj-example.php
+++ templates/mj-example.php
@@ -1,97 +1,97 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title><?= $page->title() ?></title>
-	<?= css('assets/plugins/kirby-mailjet/css/pastie.css') ?>
-	<style>
-		pre, form { margin: 40px 0px; }
-		span.variable, span.array, span.list { background-color: rgb(0, 119, 0);;
+    <title><?= $page->title() ?></title>
+    <?= css('assets/plugins/kirby-mailjet/css/pastie.css') ?>
+    <style>
+        pre, form { margin: 40px 0px; }
+        span.variable, span.array, span.list { background-color: rgb(0, 119, 0);;
 color: rgb(255, 255, 255); }
-		span.comment { background-color: rgb(255, 240, 240);
+        span.comment { background-color: rgb(255, 240, 240);
 color: rgb(221, 34, 0);}
 
-		.uniform__potty {
-		    position: absolute;
-		    left: -9999px;
-		}
-		form { padding: 40px;  background-color: #EEEEEE;  border: 1px solid #DEDEDE; }
-		form label { display: inline-block; margin-top: 20px; width: 100px; margin-right: 10px; }
-		form input { display: inline-block; margin-top: 20px; width: calc(100% - 140px); height: 20px;}
-		form input[type=submit] {width: calc(100% - 20px); line-height: 20px; }
-		form input.error { background-color: red; color: white; }
-		form .error-text { margin: 10px 0; padding-left: 120px; font-size: 0.8em; color: red; }
+        .uniform__potty {
+            position: absolute;
+            left: -9999px;
+        }
+        form { padding: 40px;  background-color: #EEEEEE;  border: 1px solid #DEDEDE; }
+        form label { display: inline-block; margin-top: 20px; width: 100px; margin-right: 10px; }
+        form input { display: inline-block; margin-top: 20px; width: calc(100% - 140px); height: 20px;}
+        form input[type=submit] {width: calc(100% - 20px); line-height: 20px; }
+        form input.error { background-color: red; color: white; }
+        form .error-text { margin: 10px 0; padding-left: 120px; font-size: 0.8em; color: red; }
 
-	</style>
+    </style>
 </head>
 <body style="max-width: 1000px;margin:0 auto;font-family:'Helvetica Neue', Helvetica,Arial, sans-serif;">
-	<center>
-		<h1 style="margin-top:40px;"><?= $page->title() ?></h1>
-		<div><a style="border-radius: 5px;border:1px solid #666;color:#666;text-decoration:none;padding:5px;" target="_blank" href="<?= $site->url() ?>/panel/pages/<?= $page->diruri() ?>/edit">Edit in Panel</a> <a style="border-radius: 5px;border:1px solid #666;color:#666;text-decoration:none;padding:5px;" target="_blank" href="https://github.com/bnomei/kirby-mailjet">Github Docs</a>
-		<a style="border-radius: 5px;border:1px solid #666;color:#666;text-decoration:none;padding:5px;" target="_blank" href="https://mjml.io/try-it-live">mjml Online Editor</a>
-		<br><br></div>
-	</center>
+    <center>
+        <h1 style="margin-top:40px;"><?= $page->title() ?></h1>
+        <div><a style="border-radius: 5px;border:1px solid #666;color:#666;text-decoration:none;padding:5px;" target="_blank" href="<?= $site->url() ?>/panel/pages/<?= $page->diruri() ?>/edit">Edit in Panel</a> <a style="border-radius: 5px;border:1px solid #666;color:#666;text-decoration:none;padding:5px;" target="_blank" href="https://github.com/bnomei/kirby-mailjet">Github Docs</a>
+        <a style="border-radius: 5px;border:1px solid #666;color:#666;text-decoration:none;padding:5px;" target="_blank" href="https://mjml.io/try-it-live">mjml Online Editor</a>
+        <br><br></div>
+    </center>
 
-	<?php if(site()->user()): ?>
-	<pre><code data-language="php"><?php print_r(['hash'=>Kirbymailjet::hash()]) ?></code></pre>
-	<?php endif; ?>
+    <?php if(site()->user()): ?>
+    <pre><code data-language="php"><?php print_r(['hash'=>Kirbymailjet::hash()]) ?></code></pre>
+    <?php endif; ?>
 
-	<pre><code data-language="mustache"><?php print_r($mustache); ?></code></pre>
+    <pre><code data-language="mustache"><?php print_r($mustache); ?></code></pre>
 
-	<pre><code data-language="mustache"><?php echo $mjmlCode ?></code></pre>
+    <pre><code data-language="mustache"><?php echo $mjmlCode ?></code></pre>
 
-	<?= js('assets/plugins/kirby-mailjet/js/rainbow-custom.min.js') ?>
-	<script>
-		Rainbow.extend('mustache', [
-			{
-		        name: 'comment',
-		        pattern: /&lt;\!--[\S\s]*?--&gt;/g
-		    },
-		    {
-		        name: 'variable',
-		        pattern: /{{([\w\ ]+)}}/gm
-		    },
-		    {
-		        name: 'array.key',
-		        pattern: /\[([\w\ ]+)\]/gm
-		    },
-		    {
-		        name: 'list.begin',
-		        pattern: /{{(\#[\w\ ]+)}}/gm
-		    },
-		    {
-		        name: 'list.end',
-		        pattern: /{{(\/[\w\ ]+)}}/gm
-		    }
-		]);
-	</script>
+    <?= js('assets/plugins/kirby-mailjet/js/rainbow-custom.min.js') ?>
+    <script>
+        Rainbow.extend('mustache', [
+            {
+                name: 'comment',
+                pattern: /&lt;\!--[\S\s]*?--&gt;/g
+            },
+            {
+                name: 'variable',
+                pattern: /{{([\w\ ]+)}}/gm
+            },
+            {
+                name: 'array.key',
+                pattern: /\[([\w\ ]+)\]/gm
+            },
+            {
+                name: 'list.begin',
+                pattern: /{{(\#[\w\ ]+)}}/gm
+            },
+            {
+                name: 'list.end',
+                pattern: /{{(\/[\w\ ]+)}}/gm
+            }
+        ]);
+    </script>
 
-	<?php if($newsletterStatus = get('newsletter')): ?>
-	<form>
-		<p><b>Status: </b><?php echo $newsletterStatus ?></p>
-	</form>
-	<?php else: ?>
-	<form action="<?php echo $page->url() ?>" method="POST">
-		<h2><i>Newsletter Test</i> Contactslist Subscription</h2>
+    <?php if($newsletterStatus = get('newsletter')): ?>
+    <form>
+        <p><b>Status: </b><?php echo $newsletterStatus ?></p>
+    </form>
+    <?php else: ?>
+    <form action="<?php echo $page->url() ?>" method="POST">
+        <h2><i>Newsletter Test</i> Contactslist Subscription</h2>
 
-		<p>You have to create <i>firstname</i> and <i>lastname</i> as <a href="https://app.mailjet.com/contacts/lists/properties" target="_blank">custom Contact-Properties in your Mailjet-Dashboard</a>.</p>
+        <p>You have to create <i>firstname</i> and <i>lastname</i> as <a href="https://app.mailjet.com/contacts/lists/properties" target="_blank">custom Contact-Properties in your Mailjet-Dashboard</a>.</p>
 
-		<label>Firstname</label>
-	    <input<?php if ($form->error('firstname')): ?> class="error"<?php endif; ?> name="firstname" type="text" value="<?php echo $form->old('firstname') ?>">
-	    <?php snippet('mj-example-form-error', ['field' => 'firstname']) ?>
+        <label>Firstname</label>
+        <input<?php if ($form->error('firstname')): ?> class="error"<?php endif; ?> name="firstname" type="text" value="<?php echo $form->old('firstname') ?>">
+        <?php snippet('mj-example-form-error', ['field' => 'firstname']) ?>
 
-	    <label>Lastname</label>
-	    <input<?php if ($form->error('lastname')): ?> class="error"<?php endif; ?> name="lastname" type="text" value="<?php echo $form->old('lastname') ?>">
-	    <?php snippet('mj-example-form-error', ['field' => 'lastname']) ?>
+        <label>Lastname</label>
+        <input<?php if ($form->error('lastname')): ?> class="error"<?php endif; ?> name="lastname" type="text" value="<?php echo $form->old('lastname') ?>">
+        <?php snippet('mj-example-form-error', ['field' => 'lastname']) ?>
 
-	    <label>Email</label>
-	    <input<?php if ($form->error('email')): ?> class="error"<?php endif; ?> name="email" type="email" value="<?php echo $form->old('email') ?>">
-	    <?php snippet('mj-example-form-error', ['field' => 'email']) ?>
+        <label>Email</label>
+        <input<?php if ($form->error('email')): ?> class="error"<?php endif; ?> name="email" type="email" value="<?php echo $form->old('email') ?>">
+        <?php snippet('mj-example-form-error', ['field' => 'email']) ?>
 
-	    <?php echo csrf_field() ?>
-	    <?php echo honeypot_field() ?>
-	    <input type="submit" value="Submit">
-	    <?php snippet('mj-example-form-error', ['field' => \Uniform\Actions\EmailAction::class]) ?>
-	</form>
-	<?php endif; ?>
+        <?php echo csrf_field() ?>
+        <?php echo honeypot_field() ?>
+        <input type="submit" value="Submit">
+        <?php snippet('mj-example-form-error', ['field' => \Uniform\Actions\EmailAction::class]) ?>
+    </form>
+    <?php endif; ?>
 </body>
 </html>
